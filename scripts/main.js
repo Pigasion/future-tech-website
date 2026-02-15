b9ad3485ca21008c01ece45e6b17a3d71b3c819a
Pigasion <aleksej.xadanovich.08@mail.ru>
2026-02-16 01:40:35 +0300

    add Video scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
 import LikeCollection from './Like.js'
+import VideoPlayerCollection from './VideoPlayer.js'
 
 new Header()
 new TabsCollection()
 new LikeCollection()
+new VideoPlayerCollection()
