80199fd87a2f25858bc2cdc8059b6a1a3a07c528
Pigasion <aleksej.xadanovich.08@mail.ru>
2026-02-20 00:06:27 +0300

    page "Blog"

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import Header from './Header.js'
 import TabsCollection from './Tabs.js'
 import LikeCollection from './Like.js'
 import VideoPlayerCollection from './VideoPlayer.js'
+import ExpandableContentCollection from './ExpandableContent.js'
 
 new Header()
 new TabsCollection()
 new LikeCollection()
 new VideoPlayerCollection()
+new ExpandableContentCollection()
