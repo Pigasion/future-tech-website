1d89da8ca385ac9a4771368925d64cce4c4ed97f
Pigasion <aleksej.xadanovich.08@mail.ru>
2026-02-28 03:05:00 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,11 @@ import TabsCollection from './Tabs.js'
 import LikeCollection from './Like.js'
 import VideoPlayerCollection from './VideoPlayer.js'
 import ExpandableContentCollection from './ExpandableContent.js'
+import InputMaskCollection from './InputMask.js'
 
 new Header()
 new TabsCollection()
 new LikeCollection()
 new VideoPlayerCollection()
 new ExpandableContentCollection()
+new InputMaskCollection()
